da02cb13748c6e426ca53095cdc9c3cf3d8c3b35
Adolfo Marinucci <adospace@msn.com>
2021-12-17 14:53:55 +0100

    Added some missing file from previous git commit

---
+++ src/MessageBox.Client/Implementation/TaskExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace MessageBox.Client.Implementation
+{
+    internal static class TaskExtensions
+    {
+        public static async Task<bool> CancelAfter(this Task task, TimeSpan timeout, CancellationToken cancellationToken = default)
+        {
+            if (await Task.WhenAny(task, Task.Delay(timeout, cancellationToken)) != task) return false;
+            
+            // Task completed within timeout.
+            // Consider that the task may have faulted or been canceled.
+            // We re-await the task so that any exceptions/cancellation is rethrown.
+            await task;
+            return true;
+
+            // timeout/cancellation logic
+        }
+    }
+}
---
+++ src/MessageBox.Testing/InMemoryBusClientOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace MessageBox.Testing
+{
+    public class InMemoryBusClientOptions : IBusClientOptions
+    {
+        public TimeSpan DefaultCallTimeout { get ; set; } = TimeSpan.FromSeconds(5);
+
+        public int MaxDegreeOfParallelism { get; set; } = 1;
+    }
+}
