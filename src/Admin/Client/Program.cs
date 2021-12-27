cc9df52450093e123a3c487dcbbb5dd7386e9772
Adolfo Marinucci <adospace@msn.com>
2021-12-27 23:55:52 +0100

    Added MudBlazor to Admin web app

--- src/Admin/Client/Program.cs
+++ src/Admin/Client/Program.cs
@@ -1,6 +1,7 @@
 using MessageBox.Server.Tcp.Host.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
+
 await builder.Build().RunAsync();
+
